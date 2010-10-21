388c49caebab991a8ad256ba4ed21fc2dd2f5edb
Darius Bacon <darius@wry.me>
2010-10-21 09:58:49 -0700

    put lengths first; more declarative doc comment

--- lz/lz77.py
+++ lz/lz77.py
@@ -11,44 +11,44 @@ First cut at LZ77 from http://en.wikipedia.org/wiki/LZ77_and_LZ78
 #. (0, 0, 'w')
 #. (0, 0, 'o')
 #. (1, 1, 'd')
-#. (5, 3, 'u')
+#. (3, 5, 'u')
 #. (0, 0, 'l')
-#. (6, 2, 'a')
-#. (13, 5, 'c')
-#. (20, 1, 'u')
-#. (3, 1, 'k')
-#. (10, 1, 'c')
-#. (6, 5, 'i')
+#. (2, 6, 'a')
+#. (5, 13, 'c')
+#. (1, 20, 'u')
+#. (1, 3, 'k')
+#. (1, 10, 'c')
+#. (5, 6, 'i')
 #. (0, 0, 'f')
-#. (21, 13, 'w')
-#. (39, 3, 'd')
+#. (13, 21, 'w')
+#. (3, 39, 'd')
 #. 
 
 def encode(s):
-    """Generate LZ77 triples encoding s. Each triple (k, n, c) means
-    look back k characters (in the characters appended so far), append
-    n characters from there, then append the single character c."""
+    """Generate LZ77 triples encoding s. The triple (n, k, c) means
+    that each of the next n characters is equal to the character k
+    places behind it; then the next character after them is c."""
     input = iter(s)
     window = ''
     try:
         while True:
             chunk = ''
-            while chunk in window:  # TODO extend window with chunk[:-1]
+            while chunk in window: # TODO: be able to output n > k
                 c = input.next()
                 chunk += c
             i = window.rindex(chunk[:-1])
-            yield len(window) - i, len(chunk)-1, c
+            yield len(chunk)-1, len(window) - i, c
             window += chunk
     except StopIteration:
         if chunk:
             i = window.rindex(chunk[:-1])
-            yield len(window) - i, len(chunk)-1, c
+            yield len(chunk)-1, len(window) - i, c
 
 def decode(triples):
     """Given an LZ77-code iterable, generate string chunks that,
     concatenated, form the original plaintext."""
     window = ''
-    for offset, length, c in triples:
+    for length, offset, c in triples:
         chunk = window[-offset:(-offset+length or None)] if length else ''
         yield chunk + c
         window += chunk + c
