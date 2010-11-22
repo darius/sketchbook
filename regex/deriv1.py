f7cbec4bf8c3fdc20e075bd29f1fd40f22dc3754
Darius Bacon <darius@wry.me>
2010-11-21 23:48:07 -0800

    ditch old class-based representation

--- regex/deriv1.py
+++ regex/deriv1.py
@@ -5,59 +5,21 @@ derivatives.
 
 def match(re, s):
     for c in s:
-        re = re.deriv(c)
+        re = re(c)
     return re.nullable
 
-class Fail:
-    nullable = False
-    def deriv(self, c):
-        return self
-fail = Fail()
+def mark(nullable, deriv):
+    deriv.nullable = nullable
+    return deriv
 
-class Empty:
-    nullable = True
-    def deriv(self, c):
-        return fail
-empty = Empty()
+fail = mark(False, lambda c: fail)
+empty = mark(True, lambda c: fail)
 
-class Lit:
-    nullable = False
-    def __init__(self, c):
-        assert len(c) == 1
-        self.c = c
-    def deriv(self, c):
-        return empty if c == self.c else fail
-
-class Alt:
-    def __init__(self, mk, re1, re2):
-        self.nullable = re1.nullable or re2.nullable
-        self.mk = mk
-        self.re1 = re1
-        self.re2 = re2
-    def deriv(self, c):
-        return self.mk.alt(self.re1.deriv(c), self.re2.deriv(c))
-
-class Seq:
-    def __init__(self, mk, re1, re2):
-        self.nullable = re1.nullable and re2.nullable
-        self.mk = mk
-        self.re1 = re1
-        self.re2 = re2
-    def deriv(self, c):
-        blocking = self.mk.seq(self.re1.deriv(c), self.re2)
-        if self.re1.nullable: return self.mk.alt(blocking, self.re2.deriv(c))
-        else: return blocking
-
-class Many:
-    nullable = True
-    def __init__(self, mk, re):
-        self.mk = mk
-        self.re = re
-    def deriv(self, c):
-        return self.mk.seq(self.re.deriv(c), self)
+def _lit(literal):
+    return mark(False, lambda c: empty if c == literal else fail)
 
 class MemoTable(dict):
-    def get(self, key, make):
+    def enter(self, key, make):
         if key not in self:
             self[key] = make()
         return self[key]
@@ -70,24 +32,38 @@ class Maker:
         self.seqs = MemoTable()
         self.stars = MemoTable()
 
-    def lit(self, c):
-        return self.lits.get(c, lambda: Lit(c))
+    def lit(self, literal):
+        return self.lits.enter(literal, lambda: _lit(literal))
 
     def alt(self, re1, re2):
         if re1 is fail: return re2
         if re2 is fail: return re1
-        return self.alts.get((re1, re2), lambda: Alt(self, re1, re2))
+        return self.alts.enter((re1, re2), lambda: self._alt(re1, re2))
+
+    def _alt(self, re1, re2):
+        return mark(re1.nullable or re2.nullable,
+                    lambda c: self.alt(re1(c), re2(c)))
 
     def seq(self, re1, re2):
         if re1 is empty: return re2
         if re2 is empty: return re1
         if re1 is fail or re2 is fail: return fail
-        return self.seqs.get((re1, re2), lambda: Seq(self, re1, re2))
+        return self.seqs.enter((re1, re2), lambda: self._seq(re1, re2))
+
+    def _seq(self, re1, re2):
+        if re1.nullable:
+            def sequence(c): return self.alt(self.seq(re1(c), re2), re2(c))
+        else:
+            def sequence(c): return self.seq(re1(c), re2)
+        return mark(re1.nullable and re2.nullable, sequence)
 
     def many(self, re):
         if re is fail or re is empty: return empty
-        return self.stars.get(re, lambda: Many(self, re))
+        return self.stars.enter(re, lambda: self._many(re))
 
+    def _many(self, re):
+        def loop(c): return self.seq(re(c), loop)
+        return mark(True, loop)
 
 ## mk = Maker()
 
@@ -138,6 +114,16 @@ class Maker:
 #. True
 ## match(complicated, 'ababaxyaxz')
 #. False
+
+## match(mk.many(mk.many(mk.lit('x'))), 'xxxx')
+#. True
+## match(mk.many(mk.many(mk.lit('x'))), 'xxxxy')
+#. False
+
+## match(mk.seq(empty, mk.lit('x')), '')
+#. False
+## match(mk.seq(empty, mk.lit('x')), 'x')
+#. True
 
 ## mk.lit('x') is mk.lit('x')
 #. True
