f42e8fa02911309f12d4340fc0c7e6d045340c03
Darius Bacon <darius@wry.me>
2011-05-22 11:03:45 -0700

    cleaner code

--- sat/nqueens.py
+++ sat/nqueens.py
@@ -3,12 +3,17 @@ The N-queens problem as a SAT problem.
 """
 
 def queens_problem(n):
-    grid = [range(1+r*n, 1+(r+1)*n) for r in range(n)]
+    board = make_board(n)
     return conjoin(
-        flatten(exactly_one(row) for row in grid),
-        flatten(at_most_one(col) for col in transpose(grid)),
-        flatten(at_most_one(diag) for diag in rising_diagonals(grid)),
-        flatten(at_most_one(diag) for diag in falling_diagonals(grid)))
+        flatten(exactly_one(row) for row in board),
+        flatten(at_most_one(col) for col in transpose(board)),
+        flatten(at_most_one(diag) for diag in rising_diagonals(board)),
+        flatten(at_most_one(diag) for diag in falling_diagonals(board)))
+
+def make_board(n):
+    """Return a 2-d array of distinct variables: each means there's a
+    queen at its position."""
+    return [range(1+r*n, 1+(r+1)*n) for r in range(n)]
 
 ## queens_problem(1)
 #. [[1]]
@@ -17,28 +22,28 @@ def queens_problem(n):
 ## queens_problem(3)
 #. [[1, 2, 3], [-1, -2], [-1, -3], [-2, -3], [4, 5, 6], [-4, -5], [-4, -6], [-5, -6], [7, 8, 9], [-7, -8], [-7, -9], [-8, -9], [-1, -4], [-1, -7], [-4, -7], [-2, -5], [-2, -8], [-5, -8], [-3, -6], [-3, -9], [-6, -9], [-8, -4], [-9, -5], [-9, -1], [-5, -1], [-6, -2], [-2, -4], [-3, -5], [-3, -7], [-5, -7], [-6, -8]]
 
-## grid1 = [[1]]
-## grid3 = [[1,2,3],[4,5,6],[7,8,9]]
-## transpose(grid3)
+## board1 = [[1]]
+## board3 = [[1,2,3],[4,5,6],[7,8,9]]
+## transpose(board3)
 #. [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
 
-def falling_diagonals(grid):
-    n = len(grid)
+def falling_diagonals(board):
+    n = len(board)
     for total in range(2*n-1):
-        yield [grid[r][c]
+        yield [board[r][c]
                for r in range(min(total+1, n))
                for c in [total-r]
                if c < n]
 
-## list(falling_diagonals(grid1))
+## list(falling_diagonals(board1))
 #. [[1]]
-## list(falling_diagonals(grid3))
+## list(falling_diagonals(board3))
 #. [[1], [2, 4], [3, 5, 7], [6, 8], [9]]
 
-def rising_diagonals(grid):
-    return falling_diagonals(grid[::-1])
+def rising_diagonals(board):
+    return falling_diagonals(board[::-1])
 
-## list(rising_diagonals(grid3))
+## list(rising_diagonals(board3))
 #. [[7], [8, 4], [9, 5, 1], [6, 2], [3]]
 
 def at_least_one(literals):
