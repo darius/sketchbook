c03b796261804278b2d7d3c1471592b7a1611cf5
Darius Bacon <darius@wry.me>
2011-05-26 15:07:27 -0700

    drop problem argument -- subsumed by index

--- sat/indexedsat.py
+++ sat/indexedsat.py
@@ -19,7 +19,7 @@ def solve(problem):
     "Return a satisfying assignment for problem, or None if impossible."
     env = {}
     index = build_index(problem)
-    return solving(problem, index, env,
+    return solving(index, env,
                    sat.problem_variables(problem),
                    on_update(problem, env, []))
 
@@ -30,7 +30,7 @@ def build_index(problem):
             index.setdefault(-literal, []).append(clause)
     return index
 
-def solving(problem, index, env, variables, unit_literals):
+def solving(index, env, variables, unit_literals):
     "Try to extend a consistent assignment for problem to a satisfying one."
     if unit_literals is 'contradiction':
         return None
@@ -40,18 +40,18 @@ def solving(problem, index, env, variables, unit_literals):
         literal, unit_literals = unit_literals[0], unit_literals[1:]
         v, value = abs(literal), (0 < literal)
         variables = removed(variables, v)
-        return assume(problem, index, env, variables, unit_literals, v, value)
+        return assume(index, env, variables, unit_literals, v, value)
     v, variables = variables[0], variables[1:]
     for value in (False, True):
         env = assign(v, value, env)
-        result = assume(problem, index, env, variables, unit_literals, v, value)
+        result = assume(index, env, variables, unit_literals, v, value)
         if result is not None:
             return result
     return None
 
-def assume(problem, index, env, variables, unit_literals, v, value):
+def assume(index, env, variables, unit_literals, v, value):
     env = assign(v, value, env)
-    return solving(problem, index, env, variables,
+    return solving(index, env, variables,
                    on_update(index.get(v if value else -v, ()),
                              env, unit_literals))
 
