d7d0a324e568d4f9ca3d3228bf8b438ec9111efa
Darius Bacon <darius@wry.me>
2011-05-27 12:14:20 -0700

    solve constrained sudoku problems

--- sat/sudoku.py
+++ sat/sudoku.py
@@ -2,6 +2,7 @@
 Generalized Sudoku as a SAT problem.
 """
 
+import math
 import sys
 sys.setrecursionlimit(5000)
 
@@ -10,25 +11,76 @@ from sat import conjoin, flatten, exactly_one, at_most_one
 from itertools import count
 
 ## sudoku(2)
-#. 4 3 2 1
-#. 2 1 4 3
-#. 3 4 1 2
-#. 1 2 3 4
+#. 4 3 | 2 1
+#. 2 1 | 4 3
+#. ---------
+#. 3 4 | 1 2
+#. 1 2 | 3 4
 #. 
 ## sudoku(3)
-#. 9 8 7 6 5 4 3 2 1
-#. 6 5 4 3 2 1 9 8 7
-#. 3 2 1 9 8 7 6 5 4
-#. 8 9 6 7 4 5 2 1 3
-#. 7 4 5 2 1 3 8 9 6
-#. 2 1 3 8 9 6 7 4 5
-#. 5 7 9 4 6 8 1 3 2
-#. 4 6 8 1 3 2 5 7 9
-#. 1 3 2 5 7 9 4 6 8
+#. 9 8 7 | 6 5 4 | 3 2 1
+#. 6 5 4 | 3 2 1 | 9 8 7
+#. 3 2 1 | 9 8 7 | 6 5 4
+#. ---------------------
+#. 8 9 6 | 7 4 5 | 2 1 3
+#. 7 4 5 | 2 1 3 | 8 9 6
+#. 2 1 3 | 8 9 6 | 7 4 5
+#. ---------------------
+#. 5 7 9 | 4 6 8 | 1 3 2
+#. 4 6 8 | 1 3 2 | 5 7 9
+#. 1 3 2 | 5 7 9 | 4 6 8
+#. 
+
+easy1 = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+hard1 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
+
+## solve(easy1)
+#. 4 8 3 | 9 2 1 | 6 5 7
+#. 9 6 7 | 3 4 5 | 8 2 1
+#. 2 5 1 | 8 7 6 | 4 9 3
+#. ---------------------
+#. 5 4 8 | 1 3 2 | 9 7 6
+#. 7 2 9 | 5 6 4 | 1 3 8
+#. 1 3 6 | 7 9 8 | 2 4 5
+#. ---------------------
+#. 3 7 2 | 6 8 9 | 5 1 4
+#. 8 1 4 | 2 5 3 | 7 6 9
+#. 6 9 5 | 4 1 7 | 3 8 2
 #. 
 
 def sudoku(n):
-    
+    rows, problem = make_sudoku_grid(n)
+    model = solver.solve(problem)
+    print_sudoku_solution(rows, model)
+
+def solve(form):
+    rows, problem = make_sudoku_grid(3)
+    constrain_to_form(problem, rows, form)
+    model = solver.solve(problem)
+    print_sudoku_solution(rows, model)
+
+def constrain_to_form(problem, rows, form):
+    form_chars = iter(form)
+    for row in rows:
+        for slot, c in zip(row, form_chars):
+            if c in '123456789':
+                digit = int(c)
+                problem.append([slot[digit-1]])
+
+def print_sudoku_solution(rows, model):
+    divider = int(math.sqrt(len(rows)))
+    for r, row in enumerate(rows):
+        if 0 < r and r % divider == 0:
+            print '-' * (2 * divider * (divider+1) - 3)
+        for c, slot in enumerate(row):
+            if 0 < c and c % divider == 0:
+                print '|',
+            for i, v in enumerate(slot):
+                if model[v]:
+                    print i+1,
+        print
+
+def make_sudoku_grid(n):
     problem = []
     def add_permutation(slots):
         for variables in transpose(slots):
@@ -52,13 +104,7 @@ def sudoku(n):
     for column in transpose(rows):
         add_permutation(column)
 
-    model = solver.solve(problem)
-    for row in rows:
-        for slot in row:
-            for i, v in enumerate(slot):
-                if model[v]:
-                    print i+1,
-        print
+    return rows, problem
 
 def transpose(grid):
     return zip(*grid)
