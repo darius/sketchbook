8209d5c7a61db883254c03135cd22371c79c9b4a
Darius Bacon <darius@wry.me>
2011-06-13 20:38:23 -0700

    move state to a Game object so we can have many

--- sat/ttypuzzle.py
+++ sat/ttypuzzle.py
@@ -19,94 +19,87 @@ import dimacs
 import sat
 
 def main():
+    # Problem from http://toughsat.appspot.com/
+    nvariables, problem = dimacs.load('problems/factoring6.dimacs')
     os.system('stty raw')
     try:
-        play()
+        play(Game(problem))
     finally:
         os.system('stty sane')
         print
 
-# Problem from http://toughsat.appspot.com/
-nvariables, problem = dimacs.load('problems/factoring6.dimacs')
-
-# Make sure it's solvable, and visible in a terminal window:
-## import indexedsat as solver
-## solver.solve(problem) is not None
-#. True
-## nvariables <= 25
-#. True
-## len(problem) < 39
-#. True
-
-env = dict((v, False) for v in sat.problem_variables(problem))
-
-def play():
+def play(game):
     sys.stdout.write(ansi.clear_screen)
     while True:
-        refresh()
-        if is_solved():
+        game.refresh()
+        if game.is_solved():
             print 'You win!'
             break
-        v = get_command()
+        cmd = sys.stdin.read(1)
+        v = game.variable_of_name(cmd)
         if v is not None:
-            flip(v)
+            game.flip(v)
         else:
             print 'You quit!'
             break
 
-def refresh():
-    sys.stdout.write(ansi.home)
-    show(coloring=True)
+class Game(object):
 
-def show(coloring=False):
-    "Display the board."
+    def __init__(self, problem):
+        self.problem = problem
+        variables = sat.problem_variables(problem)
+        self.env = dict((v, False) for v in variables)
+        self.labels = ('1234567890' + string.ascii_uppercase)[:len(variables)]
 
-    def write(color, s):
-        if coloring: sys.stdout.write(color)
-        sys.stdout.write(s)
+    def refresh(self):
+        sys.stdout.write(ansi.home)
+        self.show(coloring=True)
 
-    def present(v, clause):
-        if v in clause or -v in clause:
-            true = (env[v] if v in clause else not env[v])
-            return '*' if true else 'O'
-        else:
-            return '.'
+    def show(self, coloring=False):
+        "Display the board."
 
-    for v in sat.problem_variables(problem):
-        write(normal, labels[v-1] + ':')
-        for clause in problem:
-            write(normal if clause_is_satisfied(clause) else look_at_me,
-                  ' ' + present(v, clause))
-        write(chr(13), '\n')
-    write(normal, '')
+        def write(color, s):
+            if coloring: sys.stdout.write(color)
+            sys.stdout.write(s)
+
+        def present(v, clause):
+            if v in clause or -v in clause:
+                true = (self.env[v] if v in clause else not self.env[v])
+                return '*' if true else 'O'
+            else:
+                return '.'
+
+        for v in sat.problem_variables(self.problem):
+            write(normal, self.name_of_variable(v) + ':')
+            for clause in self.problem:
+                color = (normal if self.clause_is_satisfied(clause)
+                         else look_at_me)
+                write(color, ' ' + present(v, clause))
+            write(chr(13), '\n')
+        write(normal, '')
+
+    def name_of_variable(self, v):
+        return self.labels[v-1]
+
+    def variable_of_name(self, label):
+        try:
+            return self.labels.index(label.upper()) + 1
+        except ValueError:
+            return None
+
+    def is_solved(self):
+        return sat.is_satisfied(self.problem, self.env)
+
+    def clause_is_satisfied(self, clause):
+        return any(self.env.get(abs(literal)) == (0 < literal)
+                   for literal in clause)
+
+    def flip(self, v):
+        self.env[v] = not self.env[v]
+
 
 normal     = ansi.set_foreground(ansi.black) # Assuming a white background
 look_at_me = ansi.set_foreground(ansi.red)
-
-def get_command():
-    c = sys.stdin.read(1)
-    return variable_of_name(c)
-
-def name_of_variable(v):
-    return labels[v-1]
-
-def variable_of_name(label):
-    try:
-        return labels.index(label.upper()) + 1
-    except ValueError:
-        return None
-
-labels = ('1234567890' + string.ascii_uppercase)[:nvariables]
-
-def is_solved():
-    return sat.is_satisfied(problem, env)
-
-def clause_is_satisfied(clause):
-    return any(env.get(abs(literal)) == (0 < literal)
-               for literal in clause)
-
-def flip(v):
-    env[v] = not env[v]
 
 
 if __name__ == '__main__':
