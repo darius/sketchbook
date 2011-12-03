74d191bf866cfaf81e4c767c2647976e0e7fea33
Darius Bacon <darius@wry.me>
2011-12-02 18:22:54 -0800

    Allow different-length tuples, for now. Tighten the code.

--- rete/production.py
+++ rete/production.py
@@ -1,11 +1,9 @@
 """
 Really basic production-system interpreter.
 
-A fact is a 3-tuple with no variables.
-A pattern is a 3-tuple possibly including variables.
+A fact is a tuple with no variables.
+A pattern is a tuple possibly including variables.
 A template is like a pattern.
-
-(Actually n-tuples but the n's must all be the same.)
 
 A rule is a pair (patterns, templates). A rule fires when the patterns
 match some facts; the effect is to add new facts by filling out the
@@ -63,7 +61,7 @@ def run(rules, initial_facts):
     def consequences((guard, action)):
         for env in matching(guard, facts, [{}]):
             for template in action:
-                yield fill_out(template, env)
+                yield fill(template, env)
 
     def new_consequences(rule):
         for fact in consequences(rule):
@@ -77,30 +75,17 @@ def run(rules, initial_facts):
         else:
             break
 
-def flatmap(f, xs):
-    for x in xs:
-        for result in f(x):
-            yield result
-
 def matching(patterns, facts, envs):
     "Yield all ways of extending an env to match all patterns conjointly."
-    if not patterns:
-        return envs
-    else:
-        return matching(patterns[1:], facts,
-                        matches(patterns[0], facts, envs))
-
-def matches(pattern, facts, envs):
-    "Yield all ways of extending an env to match pattern."
-    return successful(match(pattern, fact, env)
-                      for env in envs for fact in facts)
-
-def successful(xs):
-    return (x for x in xs if x is not None)
+    def matches(pattern, envs):
+        return successful(match(pattern, fact, env)
+                          for env in envs for fact in facts)
+    return foldr(matches, patterns, envs)
 
 def match(pattern, fact, env):
     "Return an extended env matching pattern to fact, or None if impossible."
-    assert len(pattern) == len(fact)
+    if len(pattern) != len(fact):
+        return None
     for x, y in zip(pattern, fact):
         if is_variable(x):
             if x not in env:
@@ -111,12 +96,7 @@ def match(pattern, fact, env):
             return None
     return env
 
-def extend(env, var, val):
-    result = dict(env)
-    result[var] = val
-    return result
-
-def fill_out(template, env):
+def fill(template, env):
     "Instantiate template's variables from env."
     return [env[x] if is_variable(x) else x
             for x in template]
@@ -126,3 +106,21 @@ def is_variable(x):
 
 def is_fact(clause):
     return not any(is_variable(x) for x in clause)
+
+def extend(env, var, val):
+    result = dict(env)
+    result[var] = val
+    return result
+
+def flatmap(f, xs):
+    for x in xs:
+        for result in f(x):
+            yield result
+
+def foldr(f, xs, z):
+    for x in reversed(xs):
+        z = f(x, z)
+    return z
+
+def successful(xs):
+    return (x for x in xs if x is not None)
