f031af4c5cd66cfa14a7d10e92b93386a6fe6853
Darius Bacon <darius@wry.me>
2011-12-02 23:17:49 -0800

    Fix bug: main loop never repeated. Add fresh-variable feature. Add append example.

--- rete/production.py
+++ rete/production.py
@@ -37,6 +37,53 @@ mom cersei myrcella
 #. grandparent tywin myrcella
 #. 
 
+append_rules = """\
+append Xs Ys Zs
+Xs nil
+Ys nil
+--> 
+    Zs nil
+
+append Xs Ys Zs
+Xs nil
+Ys cons Y Ys'
+--> 
+    Zs cons Y Zs'
+    append Xs Ys' Zs'
+
+append Xs Ys Zs
+Xs cons X Xs'
+--> 
+    Zs cons X Zs'
+    append Xs' Ys Zs'
+"""
+
+append_facts = """\
+a0 cons hello a1
+a1 cons world a2
+a2 nil
+b0 cons goodbye b1
+b1 cons cruel b2
+b2 cons world b3
+b3 nil
+append a0 b0 z0
+"""
+
+## main(append_rules, append_facts)
+#. 
+#. z0 cons hello :Zs'.2.0.7
+#. append a1 b0 :Zs'.2.0.7
+#. :Zs'.2.0.7 cons world :Zs'.2.1.10
+#. append a2 b0 :Zs'.2.1.10
+#. :Zs'.2.1.10 cons goodbye :Zs'.1.3.2.12
+#. append a2 b1 :Zs'.1.3.2.12
+#. :Zs'.1.3.2.12 cons cruel :Zs'.1.4.2.14
+#. append a2 b2 :Zs'.1.4.2.14
+#. :Zs'.1.4.2.14 cons world :Zs'.1.5.2.16
+#. append a2 b3 :Zs'.1.5.2.16
+#. :Zs'.1.5.2.16 nil
+#. 
+
 def main(rules_text, facts_text):
     for fact in run(parse_rules(rules_text), parse_factoids(facts_text)):
         print ' '.join(fact)
@@ -45,7 +92,13 @@ def parse_rules(text):
     return map(parse_rule, text.split('\n\n'))
 
 def parse_rule(text):
-    return map(parse_factoids, text.split('-->', 1))
+    return make_rule(map(parse_factoids, text.split('-->', 1)))
+
+def make_rule((patterns, templates)):
+    return patterns, templates, free_vars(templates) - free_vars(patterns)
+
+def free_vars(factoids):
+    return set(x for f in factoids for x in f if is_variable(x))
 
 def parse_factoids(text):
     "Parse a list of facts, patterns, or templates."
@@ -58,32 +111,40 @@ def run(rules, initial_facts):
 
     facts = list(initial_facts)
 
-    def consequences((guard, action)):
-        for env in match_all(guard, facts, [{}]):
+    def consequences((rule_num, (guard, action, fresh_vars))):
+        for env in match_all(guard, facts, [{'#': str(rule_num)}]):
+            for v in fresh_vars:
+                env = extend(env, v, make_fresh_id(v, env['#']))
             for template in action:
                 yield fill(template, env)
 
-    def new_consequences(rule):
-        for fact in consequences(rule):
+    def new_consequences(numbered_rule):
+        for fact in consequences(numbered_rule):
             if fact not in facts:
                 facts.append(fact)
                 yield fact
 
-    while True:
-        for fact in flatmap(new_consequences, rules):
+    more = True
+    while more:
+        more = False
+        for fact in flatmap(new_consequences, enumerate(rules)):
             yield fact
-        else:
-            break
+            more = True
+
+def make_fresh_id(v, tag):
+    return ':%s.%s' % (v, tag)
 
 def match_all(patterns, facts, envs):
     "Yield all ways of extending an env to match all patterns conjointly."
     def matches(pattern, envs):
-        return successful(match(pattern, fact, env)
-                          for env in envs for fact in facts)
+        return successful(match(pattern, f, fact, env)
+                          for env in envs for f, fact in enumerate(facts))
     return foldr(matches, patterns, envs)
 
-def match(pattern, fact, env):
-    "Return an extended env matching pattern to fact, or None if impossible."
+def match(pattern, f, fact, env):
+    """Return an extended env matching pattern to fact, or None if
+    impossible. If matched, append the fact-number f to result['#'] --
+    a record of dependencies."""
     if len(pattern) != len(fact):
         return None
     for x, y in zip(pattern, fact):
@@ -94,7 +155,7 @@ def match(pattern, fact, env):
             x = env[x]
         if x != y:
             return None
-    return env
+    return extend(env, '#', '%s.%s' % (env['#'], f))
 
 def fill(template, env):
     "Instantiate template's variables from env."
