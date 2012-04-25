b62b1d38f2ea3f6be4037cce76084efd7a478092
Darius Bacon <darius@wry.me>
2012-04-25 08:02:01 -0700

    drop 'take'

--- parsing/monadplus.py
+++ parsing/monadplus.py
@@ -1,13 +1,12 @@
 """
-Monadic parser combinators, overloading Python's built-in operators.
-Now the value must be a tuple -- I guess this makes it a MonadPlus.
+Parser combinators, overloading Python's built-in operators.
+PEG, always producing a tuple of values (or failure).
 TODO: change append to deal in tuples instead of lists, now that it's working
 TODO: error localization, stream input, memoizing
 TODO: for the results use a tuple-like type with constant-time append
 TODO: read http://gbracha.blogspot.com/2007/01/parser-combinators.html
            http://en.wikibooks.org/wiki/Haskell/Understanding_arrows
            http://www.haskell.org/haskellwiki/Arrow#Parser
-TODO: see if dropping 'take' can make things simpler/faster
 
 Available operators:
 # ~p +p -p p.foo repr(p)
@@ -57,10 +56,22 @@ class Peg:
     def plus(self):  return append(self, self.star())
     def maybe(self): return append(self, nil) | nil
 
-def alt(p, q): return Peg(lambda s: p(s) or q(s))
+def epsilon(vals): return Peg(lambda s: [(the(tuple, vals), s)])
 
-def seq(p, q):
-    return take(p, lambda p_vals: take(q, lambda q_vals: give(p_vals + q_vals)))
+def alt(p, q): return Peg(lambda s: p(s) or q(s))
+def seq(p, q): return combine(p, q, lambda p_vals, q_vals: p_vals + q_vals)
+
+def append(peg, rest_peg):
+    return combine(peg, rest_peg, lambda vals, (rest,):
+             singleton(list(the(tuple, vals)) + the(list, rest)))
+
+def combine(p, q, f): return Peg(lambda s: [(f(p_vals, q_vals), s2)
+                                            for p_vals, s1 in p(s)
+                                            for q_vals, s2 in q(s1)])
+
+def singleton(x): return (x,)
+
+nil = epsilon(singleton([]))
 
 def recur(f):
     peg = delay(lambda: f(peg))
@@ -72,21 +83,6 @@ def delay(thunk):
         return peg(s)
     peg = Peg(memo_thunk)
     return peg
-
-# 'give' and 'take' are the monad operators usually named 'return' and 'bind'.
-# E.g.: take(give(('hello',)), lambda vals: ...) binds vals to ('hello',)
-def give(vals):
-    return Peg(lambda s: [(the(tuple, vals), s)])
-def take(peg, f):
-    return Peg(lambda s: [res for vals, s1 in peg(s) for res in f(the(tuple, vals))(s1)])
-
-def singleton(x): return (x,)
-
-nil = give(singleton([]))
-
-def append(peg, rest_peg):
-    return take(peg, lambda vals: take(rest_peg, lambda (rest,):
-             give(singleton(list(the(tuple, vals)) + the(list, rest)))))
 
 
 # Smoke test
