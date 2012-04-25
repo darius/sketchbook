13d2e18f7f3d98b591b7ab79f2f8ab12647fdf16
Darius Bacon <darius@wry.me>
2012-04-25 05:51:40 -0700

    cleanup

--- parsing/monadplus.py
+++ parsing/monadplus.py
@@ -8,6 +8,14 @@ TODO: read http://gbracha.blogspot.com/2007/01/parser-combinators.html
            http://en.wikibooks.org/wiki/Haskell/Understanding_arrows
            http://www.haskell.org/haskellwiki/Arrow#Parser
 TODO: see if dropping 'take' can make things simpler/faster
+
+Available operators:
+# ~p +p -p p.foo repr(p)
+# & / // << >> % * - + ** ^
+# __and__ __div__ __floordiv__ __getattribute__ __lshift__ __mod__ __mul__ __neg__ __pos__ __pow__ __repr__ __rshift__ __sub__ __xor__
+
+Glossary:
+ s: input string
 """
 
 import re
@@ -15,9 +23,10 @@ import re
 def parse(peg, s):
     """Return the value from peg parsing (a prefix of) s, or None on failure.
     This function is only for single-value results."""
-    for (value,), rest in as_peg(peg)(s): return value
+    for (value,), rest in as_peg(peg)(s):
+        return value
 
-def a(T, x):
+def the(T, x):
     assert isinstance(x, T), "%s is not a %s" % (x, T)
     return x
 
@@ -54,10 +63,10 @@ def seq(p, q):
     return take(p, lambda p_vals: take(q, lambda q_vals: give(p_vals + q_vals)))
 
 def recur(f):
-    peg = delayed(lambda: f(peg))
+    peg = delay(lambda: f(peg))
     return peg
 
-def delayed(thunk):
+def delay(thunk):
     def initial_parse_fn(s):
         peg.__call__ = as_peg(thunk())
         return peg(s)
@@ -67,22 +76,17 @@ def delayed(thunk):
 # 'give' and 'take' are the monad operators usually named 'return' and 'bind'.
 # E.g.: take(give(('hello',)), lambda vals: ...) binds vals to ('hello',)
 def give(vals):
-    return Peg(lambda s: [(a(tuple, vals), s)])
+    return Peg(lambda s: [(the(tuple, vals), s)])
 def take(peg, f):
-    return Peg(lambda s: [res for vals, s1 in peg(s) for res in f(a(tuple, vals))(s1)])
+    return Peg(lambda s: [res for vals, s1 in peg(s) for res in f(the(tuple, vals))(s1)])
 
 def singleton(x): return (x,)
 
 def cons(peg, rest_peg):
     return take(peg, lambda vals: take(rest_peg, lambda (rest,):
-             give(singleton(list(a(tuple, vals)) + a(list, rest)))))
+             give(singleton(list(the(tuple, vals)) + the(list, rest)))))
 
 nil = give(singleton([]))
-
-# ~p +p -p p.foo repr(p)
-# & / // << >> % * - + ** ^
-# __and__ __div__ __floordiv__ __getattribute__ __lshift__ __mod__ __mul__ __neg__ __pos__ __pow__ __repr__ __rshift__ __sub__ __xor__
-
 
 
 # Smoke test
@@ -101,28 +105,22 @@ nil = give(singleton([]))
 ## parse(match(r'(.)').star(), 'hello')
 #. ['h', 'e', 'l', 'l', 'o']
 
+
 # Example
-
-def identity(x): return x
-def ignore(*args): pass
-
 
 def make_var(v):      return v
 def make_lam(v, e):   return '(lambda (%s) %s)' % (v, e)
 def make_app(e1, e2): return '(%s %s)' % (e1, e2)
 
-
 def test(string):
     _ = match(r'\s*')  # TODO add comments
     identifier = match(r'([A-Za-z_]\w*)\b\s*')
 
-    E     = delayed(lambda: 
+    V     =   identifier +_
+    E     = delay(lambda: 
               V                            >> make_var
             | r'\\' +_+ V + '[.]' +_+ E    >> make_lam
             | '[(]' +_+ E + E + '[)]' +_   >> make_app)
-
-    V     =   identifier +_
-
     start =   _+ E
 
     return parse(start, string)
