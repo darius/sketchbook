8d8b3eb4ff5c0a68522714c8a80fd657055fff9f
Darius Bacon <darius@wry.me>
2012-04-28 14:27:40 -0700

    make complement work; change parser interface

--- parsing/crusoemonoidal.py
+++ parsing/crusoemonoidal.py
@@ -4,48 +4,55 @@ Newer variant on crusoeparse, with an infusion of monoidal.py.
 
 import re
 
-def Peg(x):
-    if isinstance(x, (str, unicode)): return regex_parser(x)
-    if callable(x):                   return thunk_parser(x)
+def parse(x, s):
+#    print 'parse', x, s
+    p = x() if callable(x) else x
+    if isinstance(p, (str, unicode)): return parse_regex(p, s)
+    if isinstance(p, list):           return parse_rules(p, s)
+    if callable(p):                   return p(s)
     raise ValueError("Not coerceable to a peg", x)
 
-def thunk_parser(thunk):
+def parse_rules(rules, s):
 
-    def parse(s):
-        for rule in thunk():
-            result = parse_rule(rule, s)
-            if result: return result
-        return []
+    def parse_rule(rule, s):
+        vals, peggables, act = [], rule[:-1], rule[-1]
+        for p in peggables:
+            p_vals, s = parse(p, s)
+            if s is None: return (), None
+            vals.extend(p_vals)
+        r_vals = () if act is ignore else (act(*vals),)
+        return r_vals, s
 
-    def parse_rule(parts, s):
-        vals, factors, act = [], parts[:-1], parts[-1]
-        for factor in factors:
-            for f_vals, s in Peg(factor)(s):
-                vals.extend(f_vals)
-                break
-            else:
-                return []
-        rvals = () if act is ignore else (act(*vals),)
-        return [(rvals, s)]
-
-    return parse
+    for rule in rules:
+        vals, s1 = parse_rule(rule, s)
+        if s1 is not None: return vals, s1
+    return (), None
 
 def ignore(*vals): return ()
 
-def regex_parser(regex):
-    return lambda s: [(m.groups(), s[m.end():])
-                      for m in [re.match(regex, s)] if m]
+def parse_regex(regex, s):
+    m = re.match(regex, s)
+    return (m.groups(), s[m.end():]) if m else ((), None)
 
-def complement(factor):
-    return lambda: lambda s: [] if Peg(factor)(s) else [((), s)]
+def complement(peggable):
+    return lambda: lambda s: (((), s) if parse(peggable, s)[1] is None
+                              else ((), None)
 
 
 # Smoke test
 
 def identity(x): return x
 
-def a(): return [['(x)', b, 'y',   identity]]
-def b(): return [['z',             ignore]]
+def a(): return [['(x)', c, 'y',         identity]]
+def b(): return [['z',                   ignore]]
+def c(): return [[complement('z'), '.',  ignore]]
 
-## Peg(a)('xzyu')
-#. [(('x',), 'u')]
+## parse('x', 'y')
+#. ((), None)
+## parse(complement('x'), 'y')
+#. ((), 'y')
+
+## parse(a, 'xayu')
+#. (('x',), 'u')
+## parse(a, 'xzyu')
+#. ((), None)
