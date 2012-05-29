f796bb2d9dadc9194b0d430b2ab57099a5c47ff0
Darius Bacon <darius@wry.me>
2012-05-29 00:57:17 -0700

    reorg to flow better; add doc

--- boole/validity_iter.py
+++ boole/validity_iter.py
@@ -1,5 +1,9 @@
 """
 Iterative version of validity.py
+Concrete representation of expressions:
+   bool: constant
+   str:  variable
+   tuple (test_expr, if_true_expr, if_false_expr): if-then-else
 """
 
 isa = isinstance
@@ -16,15 +20,22 @@ def satisfy(expr, value):
     env = Env()
     fail, succeed = [], []
     while True:
-        if isa(expr, bool):
-            if expr == value:
-                if not succeed: return env
-                expr, value = succeed.pop()
-            else:
+        if isa(expr, tuple):
+            test, if_true, if_false = expr
+            # TODO: it's expensive to copy the succeed stack.
+            #       Can we just save its depth instead?
+            fail.append((test, env, if_true, value, list(succeed)))
+            succeed.append((if_false, value))
+            expr, value = test, False
+        elif isa(expr, bool):
+            if expr != value:
                 if not fail: return False
                 test, env, if_true, value, succeed = fail.pop()
                 succeed.append((if_true, value))
                 expr, value = test, True
+            else:
+                if not succeed: return env
+                expr, value = succeed.pop()
         elif isa(expr, str):
             if expr in env:
                 expr = env[expr]
@@ -32,13 +43,8 @@ def satisfy(expr, value):
                 env = env.extend(expr, value)
                 if not succeed: return env
                 expr, value = succeed.pop()
-        else: # if-then-else expression
-            test, if_true, if_false = expr
-            # TODO: it's expensive to copy the succeed stack.
-            #       Can we just save its depth instead?
-            fail.append((test, env, if_true, value, list(succeed)))
-            succeed.append((if_false, value))
-            expr, value = test, False
+        else:
+            assert False
 
 def and_(x, y): return (x, y, False)
 def or_(x, y):  return (x, True, y)
