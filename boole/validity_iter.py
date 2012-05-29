a2b8c845f491c214b9e6a3439af298f76d1754f8
Darius Bacon <darius@wry.me>
2012-05-29 00:47:40 -0700

    check validity iteratively

---
+++ boole/validity_iter.py
@@ -0,0 +1,65 @@
+"""
+Iterative version of validity.py
+"""
+
+isa = isinstance
+
+def is_valid(expr): return satisfy(expr, 0) == False
+
+class Env(dict):
+    def extend(self, var, value):
+        result = Env(self)
+        result[var] = value
+        return result
+
+def satisfy(expr, value):
+    env = Env()
+    fail, succeed = [], []
+    while True:
+        if isa(expr, bool):
+            if expr == value:
+                if not succeed: return env
+                expr, value = succeed.pop()
+            else:
+                if not fail: return False
+                test, env, if_true, value, succeed = fail.pop()
+                succeed.append((if_true, value))
+                expr, value = test, True
+        elif isa(expr, str):
+            if expr in env:
+                expr = env[expr]
+            else:
+                env = env.extend(expr, value)
+                if not succeed: return env
+                expr, value = succeed.pop()
+        else: # if-then-else expression
+            test, if_true, if_false = expr
+            # TODO: it's expensive to copy the succeed stack.
+            #       Can we just save its depth instead?
+            fail.append((test, env, if_true, value, list(succeed)))
+            succeed.append((if_false, value))
+            expr, value = test, False
+
+def and_(x, y): return (x, y, False)
+def or_(x, y):  return (x, True, y)
+def not_(x):    return (x, False, True)
+def impl(x, y): return (x, y, True)
+
+x, y = 'x y'.split()
+
+## is_valid(False), is_valid(True), is_valid(x)
+#. (False, True, False)
+
+## satisfy(x, True)
+#. {'x': True}
+## satisfy(not_(x), True)
+#. {'x': False}
+## satisfy(and_(x, x), True)
+#. {'x': True}
+## satisfy(and_(x, not_(x)), True)
+#. False
+
+## is_valid(impl(impl(impl(x, y), x), x))  # Pierce's Law
+#. True
+## is_valid(impl(impl(impl(x, y), x), y))
+#. False
