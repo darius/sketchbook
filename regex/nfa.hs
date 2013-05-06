33224fb8d44c694979c483f367068b065051584a
Darius Bacon <darius@wry.me>
2013-05-06 10:49:51 -0700

    slight cleanup

--- regex/nfa.hs
+++ regex/nfa.hs
@@ -1,8 +1,13 @@
 -- Like nfa.py
 -- Uses an algebraic datatype instead of functions for the states,
 -- since we can't compare functions in Haskell.
+-- Example:
+--   match (lit 'a' . many (lit 'b' `alt` lit 'c') . lit 'd') "abbcd"
 
 import Data.Set (elems, empty, singleton, union, unions)
+
+match re = any isAccepting . elems . foldl step (singleton $ re Accept)
+  where step states c = unions $ map (after c) (elems states)
 
 data State = Accept | Expect Char State | Fork State State
    deriving (Eq, Ord)
@@ -15,10 +20,6 @@ after c Accept            = empty
 after c (Expect c' state) = if c' == c then singleton state else empty
 after c (Fork s1 s2)      = after c s1 `union` after c s2
 
-epsilon state = state
+lit = Expect
 alt re1 re2 state = re1 state `Fork` re2 state
 many re state = loop where loop = state `Fork` re loop
-lit = Expect
-
-match re = any isAccepting . elems . foldl step (singleton $ re Accept)
-  where step states c = unions $ map (after c) (elems states)
