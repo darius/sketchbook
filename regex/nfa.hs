262745a70fbbf20877d9687bd3136bb4c2fa94c0
Darius Bacon <darius@wry.me>
2013-05-12 12:40:53 -0700

    use nub instead of Set (probably less efficient, though)

--- regex/nfa.hs
+++ regex/nfa.hs
@@ -3,10 +3,9 @@
 -- since we can't compare functions in Haskell.
 -- E.g.: (lit 'a' . many (lit 'b' `alt` lit 'c') . lit 'd') `matches` "abbcd"
 
-import Data.Set (elems, empty, singleton, union, unions)
+import Data.List (nub)
 
 matches re = any accepts . foldl step [re Accept]
-  where step states c = elems $ unions $ map (after c) states
 
 data State = Accept | Expect Char State | Fork State State
   deriving (Eq, Ord)
@@ -15,9 +14,11 @@ accepts Accept       = True
 accepts (Expect _ _) = False
 accepts (Fork s1 s2) = accepts s1 || accepts s2
 
-after _ Accept            = empty
-after c (Expect c' state) = if c' == c then singleton state else empty
-after c (Fork s1 s2)      = after c s1 `union` after c s2
+step states c = nub $ concat $ map (after c) states
+
+after _ Accept            = []
+after c (Expect c' state) = if c' == c then [state] else []
+after c (Fork s1 s2)      = after c s1 ++ after c s2
 
 lit = Expect
 alt re1 re2 state = re1 state `Fork` re2 state
