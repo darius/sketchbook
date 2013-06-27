104a9aac17e7acaba6c84f9be053bf723688b706
Darius Bacon <darius@wry.me>
2013-06-27 15:57:35 -0700

    extract assembler

--- regex/ibm7094/assembler.py
+++ regex/ibm7094/assembler.py
@@ -1,20 +1,5 @@
 """
-Example: txh fail, 1, -'A'
-The -'A' must be the D field, right?
-Is 1 the T field?
-And fail the Y?
-Seems like it, except the - sign is odd:
-so: branchop Y, T, D
-txh Transfer on indeX High (:= hi_op = 3) -> ((D < XR[T]) -> IC <- Y)
-But it looks like the index registers are holding the - of values
-in general...
-
-insn   insn<S, 1:35>    I guess S means separate sign bit
-op     op<S, 1:11> := insn<S, 1:11>
-hi_op  hi_op<0:2> := insn<S, 1, 2>
-D      D<3:17> := insn<3:17>   decrement part of insn
-T      T<18:20> := insn<18:20>
-Y      Y<21:35> := insn<21:35>
+Generic 2-pass assembler.
 """
 
 from collections import defaultdict
@@ -28,18 +13,16 @@ from collections import defaultdict
 ## eval('hi', {}, UnionDict(d, defaultdict(int)))
 #. 0
 
-def toplevel(filename):
-    env = {}
-    words = assemble(list(open(filename)), env)
-    for word in words:
-        print '%012o' % word
-
-def assemble(lines, env, origin=0):
+def assemble(assemble1, lines, env, origin=0):
+    lines = list(lines)
     forward_refs = defaultdict(int)
-    assembler_pass(lines, UnionDict(env, forward_refs), origin)
-    if any(key not in env for key in forward_refs):
+    for tokens in assembler_pass(lines, UnionDict(env, forward_refs), origin):
+        assemble1(tokens, env)
+    if set(forward_refs) - set(env):
+    #if any(key not in env for key in forward_refs):
         raise Exception("Unresolved ref")
-    return assembler_pass(lines, env, origin)
+    return [assemble1(tokens, env) 
+            for tokens in assembler_pass(lines, env, origin)]
 
 ## ',4'.split(',')
 #. ['', '4']
@@ -64,7 +47,7 @@ class UnionDict(object):
             return self.dict2.get(key, default)
 
 def assembler_pass(lines, env, origin):
-    words, here = [], origin
+    here = origin
     for line in lines:
         label, tokens = tokenize(line)
         if label:
@@ -74,9 +57,8 @@ def assembler_pass(lines, env, origin):
         if not tokens:
             continue
         env['__here__'] = here
-        words.append(assemble1(tokens, env))
+        yield tokens
         here += 1
-    return words
 
 def starts_comment(string):
     return string.startswith(';')
@@ -92,64 +74,3 @@ def tokenize(line):
             tokens = tokens[:i]
             break
     return label, tokens
-
-def assemble1(tokens, env={}):
-    mnemonic, rest = tokens[0].upper(), ' '.join(tokens[1:])
-    fields = [field.strip() for field in rest.split(',')]
-    # TODO: resolve '*' as __here__
-    # TODO: what is '**'? seems to mean just 0, weird.
-    args = [eval(operand, {}, env) for operand in fields[1:]]
-    if mnemonic in branch_mnemonics:
-        return encode_branch(branch_mnemonics[mnemonic], *args)
-    else:
-        return encode(mnemonics[mnemonic], *args)
-
-
-## assemble([' acl 0,1', 'foo CLA '], {})
-#. ([4043309057L, 5368709120L], {'__here__': 1, 'foo': 1})
-
-branch_mnemonics = dict(TXI=1, TXH=3,
-                        TXL=7) # -3 'really'
-mnemonics = dict(ACL=0361,
-                 AXC=-0774,       # XXX
-                 CAL=-0500,       # XXX
-                 CLA=0500,
-                 LAC=0535,
-                 PAC=0737,
-                 PCA=0756,
-                 SCA=0636,
-                 SLW=0602,
-                 TRA=020,
-                 TSX=074)
-
-def encode(op, Y=0, T=0):
-    word = 0
-    word = word_set(word, 0, 11, op)
-    word = word_set(word, 18, 20, T)
-    word = word_set(word, 21, 35, Y)
-    return word
-
-def encode_branch(hi_op, Y=0, T=0, D=0):
-    word = 0
-    word = word_set(word,  0,  2, hi_op)
-    word = word_set(word,  3, 17, D)
-    word = word_set(word, 18, 20, T)
-    word = word_set(word, 21, 35, Y)
-    return word
-
-def word_set(word, left, right, value):
-    shift, mask = word_field(left, right)
-    return (word & ~mask) | ((value << shift) & mask)
-
-def word_bits(word, left, right):
-    shift, mask = word_field(left, right)
-    return (word & mask) >> shift
-
-def word_field(left, right):
-    shift = 35 - right
-    width = right+1 - left
-    return shift, (~(~0 << width)) << shift
-
-if __name__ == '__main__':
-    import sys
-    toplevel(sys.argv[1])
---
+++ regex/ibm7094/ibm7094asm.py
@@ -0,0 +1,87 @@
+"""
+Example: txh fail, 1, -'A'
+The -'A' must be the D field, right?
+Is 1 the T field?
+And fail the Y?
+Seems like it, except the - sign is odd:
+so: branchop Y, T, D
+txh Transfer on indeX High (:= hi_op = 3) -> ((D < XR[T]) -> IC <- Y)
+But it looks like the index registers are holding the - of values
+in general...
+
+insn   insn<S, 1:35>    I guess S means separate sign bit
+op     op<S, 1:11> := insn<S, 1:11>
+hi_op  hi_op<0:2> := insn<S, 1, 2>
+D      D<3:17> := insn<3:17>   decrement part of insn
+T      T<18:20> := insn<18:20>
+Y      Y<21:35> := insn<21:35>
+"""
+
+from assembler import assemble
+
+def toplevel(filename):
+    env = {}
+    words = assemble(assemble1, open(filename), env)
+    for word in words:
+        print '%012o' % word
+
+def assemble1(tokens, env={}):
+    mnemonic, rest = tokens[0].upper(), ' '.join(tokens[1:])
+    fields = [field.strip() for field in rest.split(',')]
+    # TODO: resolve '*' as __here__
+    # TODO: what is '**'? seems to mean just 0, weird.
+    args = [eval(operand, {}, env) for operand in fields[1:]]
+    if mnemonic in branch_mnemonics:
+        return encode_branch(branch_mnemonics[mnemonic], *args)
+    else:
+        return encode(mnemonics[mnemonic], *args)
+
+
+## assemble(assemble1, [' acl 0,1', 'foo CLA '], {})
+#. [4043309057L, 5368709120L]
+
+branch_mnemonics = dict(TXI=1, TXH=3,
+                        TXL=7) # -3 'really'
+mnemonics = dict(ACL=0361,
+                 AXC=-0774,       # XXX
+                 CAL=-0500,       # XXX
+                 CLA=0500,
+                 LAC=0535,
+                 PAC=0737,
+                 PCA=0756,
+                 SCA=0636,
+                 SLW=0602,
+                 TRA=020,
+                 TSX=074)
+
+def encode(op, Y=0, T=0):
+    word = 0
+    word = word_set(word, 0, 11, op)
+    word = word_set(word, 18, 20, T)
+    word = word_set(word, 21, 35, Y)
+    return word
+
+def encode_branch(hi_op, Y=0, T=0, D=0):
+    word = 0
+    word = word_set(word,  0,  2, hi_op)
+    word = word_set(word,  3, 17, D)
+    word = word_set(word, 18, 20, T)
+    word = word_set(word, 21, 35, Y)
+    return word
+
+def word_set(word, left, right, value):
+    shift, mask = word_field(left, right)
+    return (word & ~mask) | ((value << shift) & mask)
+
+def word_bits(word, left, right):
+    shift, mask = word_field(left, right)
+    return (word & mask) >> shift
+
+def word_field(left, right):
+    shift = 35 - right
+    width = right+1 - left
+    return shift, (~(~0 << width)) << shift
+
+if __name__ == '__main__':
+    import sys
+    toplevel(sys.argv[1])
