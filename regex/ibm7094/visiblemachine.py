c98ef9c99442c864b890718997f0f7ac9047d37c
Darius Bacon <darius@wry.me>
2013-06-29 19:51:29 -0700

    fix some basic bugs; show registers

--- regex/ibm7094/visiblemachine.py
+++ regex/ibm7094/visiblemachine.py
@@ -8,7 +8,10 @@ decimal numbers.
 
 from assembler import assemble
 
-## toplevel('thompson.vm.s')
+## v = load('thompson.vm.s')
+## v.show()
+#.                   r2                r4                r6                r8             
+#. r1                r3                r5                r7                r9             
 #. ==>set   7 0  0   20 fetch 7 0 11   40 ifne  1 A 19   60                80             
 #.  1 fetch 3 7 46   21 set   6 0  0   41 jump  4 0 11   61                81             
 #.  2 store 3 7 47   22 ifeq  7 0  0   42 noop  0 0  0   62                82             
@@ -30,12 +33,40 @@ from assembler import assemble
 #. 18 jump  0 2  1   38 jump  0 0 20   58                78                98             
 #. 19 jump  0 2  1   39 noop  0 0  0   59                79                99             
 #. 
+## v.step()
+## v.show()
+#.                   r2                r4                r6                r8             
+#. r1                r3                r5                r7            0   r9             
+#.  0 set   7 0  0   20 fetch 7 0 11   40 ifne  1 A 19   60                80             
+#. ==>fetch 3 7 46   21 set   6 0  0   41 jump  4 0 11   61                81             
+#.  2 store 3 7 47   22 ifeq  7 0  0   42 noop  0 0  0   62                82             
+#.  3 set   3 4  0   23 jump  0 0 29   43 ifne  1 B 19   63                83             
+#.  4 fetch 5 0 10   24 add   7 7 -1   44 jump  4 0 11   64                84             
+#.  5 add   3 5  0   25 fetch 3 7 66   45 found 0 0  0   65                85             
+#.  6 store 3 7 46   26 store 3 6 46   46                66                86             
+#.  7 add   7 7  1   27 add   6 6  1   47                67                87             
+#.  8 smash 7 0  0   28 jump  0 0 22   48                68                88             
+#.  9 jump  0 4  2   29 fetch 3 0 36   49                69                89             
+#. 10 jump  2 0  0   30 store 3 6 46   50                70                90             
+#. 11 set   7 0  0   31 smash 7 0  0   51                71                91             
+#. 12 set   3 4  0   32 smash 0 0 11   52                72                92             
+#. 13 fetch 5 0 10   33 getch 0 0  0   53                73                93             
+#. 14 add   3 5  0   34 jump  2 0 39   54                74                94             
+#. 15 store 3 7 66   35 jump  0 0 46   55                75                95             
+#. 16 add   7 7  1   36 jump  0 0 20   56                76                96             
+#. 17 smash 7 0 11   37 smash 0 0 11   57                77                97             
+#. 18 jump  0 2  1   38 jump  0 0 20   58                78                98             
+#. 19 jump  0 2  1   39 noop  0 0  0   59                79                99             
+#. 
 
 def toplevel(filename):
+    vm = load(filename)
+    vm.show()
+
+def load(filename):
     env = dict(('r%d'%i, i) for i in range(1, 10))
     words = assemble(assemble1, open(filename), env)
-    vm = VM(words, '')
-    vm.show()
+    return VM(words, '')
 
 def assemble1(tokens, env):
     mnemonic, rest = tokens[0].lower(), ' '.join(tokens[1:])
@@ -51,21 +82,24 @@ def assemble1(tokens, env):
         return ['%-5s%s%s%02s' % (mnemonic, args[0], args[1], int(args[2]))]
 
 def put_number(vh, vn):
+    assert len(vh) == 7
+    assert isinstance(vn, int)
     vl = '%2d' % vn
-    assert len(vl) == 2
+    assert len(vl) == 2, vn
     return vh + vl
 
 def get_number(v):
+    assert len(v) == 9
     vh, vl = v[:7], v[7:]
-    assert len(vl) == 2
-    return vh, int(vl)
+    assert len(vl) == 2, repr(v)
+    return vh, int('0' if vl == '  ' else vl)
 
 def add(u, v):
     uh, un = get_number(u)
     vh, vn = get_number(v)
     rn = (un + vn + 100) % 100
-    if not uh.strip(): return put_number(vh, rl)
-    if not vh.strip(): return put_number(uh, rl)
+    if not uh.strip(): return put_number(vh, rn)
+    if not vh.strip(): return put_number(uh, rn)
     assert False
 
 class VM(object):
@@ -80,8 +114,15 @@ class VM(object):
             self.store(put_number(' '*7, addr), value)
 
     def show(self):
+        regs = map(self.show_reg, range(10))
+        print '\n'.join(format_columns(regs, 5))
         insns = map(self.show_cell, range(100))
         print '\n'.join(format_columns(insns, 5))
+
+    def show_reg(self, i):
+        r = '  ' if i == 0 else 'r%d' % i
+        word = self.R[i]
+        return '%s %s %s %s %s' % (r, word[:5], word[5], word[6], word[7:])
 
     def show_cell(self, i):
         word = self.M[i]
@@ -89,7 +130,8 @@ class VM(object):
         return '%s%s %s %s %s' % (addr, word[:5], word[5], word[6], word[7:])
 
     def fetch(self, addr):
-        return self.M[get_number(addr)]
+        ah, an = get_number(addr)
+        return self.M[an]
 
     def store(self, addr, value):
         ah, an = get_number(addr)
@@ -108,7 +150,7 @@ class VM(object):
         op, r1, r2, addr = insn[:5], insn[5], insn[6], insn[7:]
 
         def ea():
-            return add(self.get(r2), addr)
+            return add(self.get(r2), ' '*7 + addr)
 
         if   op == 'fetch':
             self.set(r1, self.fetch(ea()))
