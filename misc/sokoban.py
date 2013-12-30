e88ca3903f9b899e50a73f5d24d9cf3493dbaa6c
Darius Bacon <darius@wry.me>
2013-12-30 10:55:03 -0300

    simplify commands

--- misc/sokoban.py
+++ misc/sokoban.py
@@ -4,7 +4,9 @@ Play Sokoban on the tty. (Use the arrow keys.)
 
 def parse(board_pic):
     lines = [line.strip() for line in board_pic.splitlines()]
-    assert lines and all(len(line) == len(lines[0]) for line in lines)
+    assert lines
+    assert all(len(line) == len(lines[0]) for line in lines)
+    assert all(line[1::2].isspace() for line in lines)
     return len(lines[0]), list(''.join(lines))
 
 def unparse((width, grid)):
@@ -23,15 +25,15 @@ def play(board):
 
 def won((width, grid)): return 'o' not in grid
 
-commands = dict(up   = lambda (width, grid): -width,
-                down = lambda (width, grid):  width,
-                left = lambda (width, grid): -2,
-                right= lambda (width, grid):  2)
+commands = dict(up   = lambda width: -width,
+                down = lambda width:  width,
+                left = lambda width: -2,
+                right= lambda width:  2)
 
 def push((width, grid), direction):
     "Update board, trying to move the player in the direction."
     i = grid.index('i' if 'i' in grid else 'I')
-    d = direction((width, grid))
+    d = direction(width)
     move(grid, 'o@', i+d, i+d+d) # First push any neighboring box.
     move(grid, 'iI', i, i+d)
 
