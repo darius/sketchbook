ecc3574dba9a6a6fa998b44bdd4110f6bffcb6a9
Darius Bacon <darius@wry.me>
2014-12-11 13:12:35 -0800

    add infix-to-forth example

--- metatoo/metametoo/testme.sh
+++ metatoo/metametoo/testme.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-./metaii.sh metaii.metaii >foo &&
-diff -u metaii.asm foo &&
+set -e
+
+./metaii.sh metaii.metaii >foo
+diff -u metaii.asm foo
 rm foo
+
+./metaii.sh infix.metaii >infix.metaii
+echo '(a+b)*c+d*d to c' | python metaii.py infix.asm - >foo
+echo ' a @ b @ + c @ * d @ d @ * + c !' >bar
+diff -u foo bar
+rm foo bar
