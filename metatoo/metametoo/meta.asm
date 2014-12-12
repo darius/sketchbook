b9e37e1278eb41afb89474cc649eb0510a3bbc01
Darius Bacon <darius@wry.me>
2014-12-12 02:10:38 -0800

    a Forthier design

---
+++ metatoo/metametoo/meta.asm
@@ -0,0 +1,158 @@
+program
+S0
+   call st
+   win_loop S0
+   read_eof
+   win_or_die
+W1
+   return
+st
+   read_id
+   if_lose L2
+   write_it
+   write_nl
+   read '='
+   win_or_die
+   call choice
+   win_or_die
+   read ';'
+   win_or_die
+   write '   return'
+   write_nl
+L2
+W3
+   return
+choice
+   call chain
+   if_lose L4
+   do_gensym
+S5
+   read '|'
+   if_lose L6
+   write '   if_win W'
+   write_top
+   write_nl
+   call chain
+   win_or_die
+L6
+W7
+   win_loop S5
+   write 'W'
+   write_pop
+   write_nl
+L4
+W8
+   return
+chain
+   call surely
+   if_lose L9
+   call tail
+   win_or_die
+L9
+   if_win W10
+   call maybe
+   if_lose L11
+   write '   if_lose '
+   do_gensym
+   write 'L'
+   write_top
+   write_nl
+   call tail
+   win_or_die
+   write 'L'
+   write_pop
+   write_nl
+L11
+W10
+   return
+tail
+S12
+   call surely
+   if_lose L13
+L13
+   if_win W14
+   call maybe
+   if_lose L15
+   write '   win_or_die'
+   write_nl
+L15
+W14
+   win_loop S12
+W16
+   return
+surely
+   read ':'
+   if_lose L17
+   read_qstring
+   if_lose L18
+   write '   write '
+   write_it
+   write_nl
+L18
+   if_win W19
+   read_id
+   if_lose L20
+   write '   write_'
+   write_it
+   write_nl
+L20
+W19
+   win_or_die
+L17
+   if_win W21
+   read '%'
+   if_lose L22
+   read_id
+   win_or_die
+   write '   do_'
+   write_it
+   write_nl
+L22
+   if_win W21
+   read '*'
+   if_lose L23
+   do_gensym
+   write 'S'
+   write_top
+   write_nl
+   call maybe
+   win_or_die
+   write '   win_loop S'
+   write_pop
+   write_nl
+L23
+W21
+   return
+maybe
+   read_qstring
+   if_lose L24
+   write '   read '
+   write_it
+   write_nl
+L24
+   if_win W25
+   read '?'
+   if_lose L26
+   read_id
+   win_or_die
+   write '   read_'
+   write_it
+   write_nl
+L26
+   if_win W25
+   read_id
+   if_lose L27
+   write '   call '
+   write_it
+   write_nl
+L27
+   if_win W25
+   read '('
+   if_lose L28
+   call choice
+   win_or_die
+   read ')'
+   win_or_die
+L28
+W25
+   return
---
+++ metatoo/metametoo/meta.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+'''
+An interpreter inspired by Val Shorre's Meta II virtual machine.
+This began as Simon Forman's code at
+http://comments.gmane.org/gmane.comp.lang.smalltalk.fonc/3642
+but it's been heavily hacked by Darius Bacon -- anything you dislike
+about this code is probably his fault.
+'''
+# Differences from Schorre's design:
+#   * Separate value and call stacks make it Forthier. The value
+#     stack and its operations replace the LABEL1/LABEL2 instructions.
+#   * The call stack includes, for helpful backtrace on error, the
+#     destination label.
+#   * Changed all the instruction names.
+#   * No ADR or END instructions. We start at address 0 and end by
+#     returning. Just s/ADR/GOTO/ in your compiler.
+#   * Also no SET instruction; subsumed by new WRITE_Q and WIN_LOOP instructions.
+#   * The WRITE instructions set the success flag.
+#   * New READ_EOF and WRITE_Q instructions.
+#   * READ_DECIMAL (the old NUM) only matches integers now.
+#   * READ_ID allows underscores.
+#   * There's a trace mode. TODO: add an instruction to turn it on or off.
+
+import itertools, re, sys
+
+def main(argv):
+    trace = False
+    if argv[1:2] == ['-trace']:
+        trace = True
+        del argv[1]
+    assert len(argv) == 3, "usage: %s [-trace] asm-file source-file" % argv[0]
+    vm = Meta_II_VM(trace)
+    vm.load(open_for_read(argv[1]).read())
+    sys.stdout.write(vm.run(open_for_read(argv[2]).read()))
+    sys.stdout.flush()
+    if vm.poisoned:
+        vm.inspect()
+    return vm.poisoned
+
+def open_for_read(filename):
+    return sys.stdin if filename == '-' else open(filename)
+
+class Meta_II_VM(object):
+    def __init__(self, trace=False):
+        self.code = []
+        self.labels = {}
+        self.trace = trace
+
+    def load(self, source_code):
+        for line in source_code.splitlines():
+            if not line or line.isspace():
+                pass
+            elif line[0].isspace():
+                self.load_line(*line.split(None, 1))
+            else:
+                label = line.strip()
+                assert label not in self.labels, "Duplicate label: " + label
+                self.labels[label] = len(self.code)
+            
+    def load_line(self, op, *args):
+        self.code.append((getattr(self, op.upper()), args))
+
+    def run(self, input_text):
+        # Appropriate terminology for a hylomorphism:
+        self.feed = input_text
+        self.bite = None
+        self.poop = ''
+        self.success = False
+        self.poisoned = False
+        self.gensym = (itertools.count()).next
+        self.calls = [-1, 'start']
+        self.stack = []
+        self.pc = 0
+        while not self.poisoned and 0 <= self.pc:
+            cur_pc = self.pc
+            op, args = self.code[self.pc]
+            self.pc += 1
+            op(*args)
+            if self.trace:
+                self.print_instruction(cur_pc, '  ' + self.state_gist())
+        return self.poop
+
+    def match(self, regex):
+        self.feed = self.feed.lstrip()
+        m = re.match(regex, self.feed)
+        if m:
+            self.eat(m.end())
+        else:
+            self.success = False
+
+    def eat(self, nchars):
+        self.success, self.bite, self.feed = True, self.feed[:nchars], self.feed[nchars:]
+
+    def decode_literal(self, arg):
+        assert arg[:1] == arg[-1:] == "'"
+        return arg[1:-1]
+
+    def write(self, string):
+        self.poop += string
+        self.success = True
+
+    # The instructions:
+
+    def READ(self, string):
+        self.match(re.escape(self.decode_literal(string)))
+
+    def READ_EOF(self):
+        self.feed = self.feed.lstrip()
+        self.success = self.feed == ''
+
+    def READ_ID(self):
+        self.match(r'[a-zA-Z_]\w*')
+
+    def READ_QSTRING(self):
+        self.match(r"'[^']*'")
+
+    def READ_DECIMAL(self):
+        self.match(r'\d+')
+
+    def GOTO(self, addr):
+        self.pc = self.labels[addr]
+
+    def IF_WIN(self, addr):
+        if self.success:
+            self.GOTO(addr)
+
+    def WIN_LOOP(self, addr):
+        self.IF_WIN(addr)
+        self.success = True
+
+    def IF_LOSE(self, addr):
+        if not self.success:
+            self.GOTO(addr)
+
+    def WIN_OR_DIE(self):
+        if not self.success:
+            self.poisoned = True
+
+    def CALL(self, addr):
+        self.calls.extend((self.pc, addr))
+        self.GOTO(addr)
+
+    def RETURN(self):
+        self.pc = self.calls[-2]
+        del self.calls[-2:]
+
+    def WRITE(self, string):
+        self.write(self.decode_literal(string))
+
+    def WRITE_Q(self):
+        self.write("'")
+
+    def WRITE_NL(self):
+        self.write('\n')
+
+    def WRITE_IT(self):
+        self.write(self.bite)
+
+    def WRITE_TOP(self):
+        self.write(str(self.stack[-1]))
+
+    def WRITE_POP(self):
+        self.write(str(self.stack.pop()))
+
+    def DO_GENSYM(self):
+        self.stack.append(self.gensym())
+
+    # Debugging/introspection.
+
+    def inspect(self):
+        print >> sys.stderr, 'stack:', self.stack
+        print >> sys.stderr, 'calls:', ' '.join(self.calls[1::2])
+        print >> sys.stderr, 'feed:', repr(self.feed[:40])
+        self.list(self.pc-2, self.pc+1)
+
+    def list(self, lo=0, hi=None):
+        for addr in range(lo, hi or len(self.code)):
+            self.print_instruction(addr)
+
+    def print_instruction(self, addr, suffix=''):
+        labels = ' '.join(k for k, v in self.labels.items() if v == addr)
+        if labels:
+            print >> sys.stderr, labels + ':'
+        op, args = self.code[addr]
+        print >> sys.stderr, ('     %3d %-18s%s'
+                              % (addr,
+                                 op.__name__.lower() + ' ' + ''.join(args),
+                                 suffix))
+
+    def state_gist(self):
+        calls = ' '.join(self.calls[3::2])
+        # TODO show the stack too -- how to format?
+        return '%s %-10r %s' % ('yn'[self.success], self.feed[:8], calls)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
