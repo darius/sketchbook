f00953bc0e45252d0c4c6125b883661140e71b54
Darius Bacon <darius@wry.me>
2016-04-30 01:41:13 -0400

    a simpler and seemingly more correct nfa.hs -- still loops on star-of-empty though

--- regex/nfa.hs
+++ regex/nfa.hs
@@ -1,26 +1,18 @@
 -- Like nfa.py
--- Uses an algebraic datatype instead of functions for the states,
--- since we can't compare functions in Haskell.
 -- E.g.: (lit 'a' . many (lit 'b' `alt` lit 'c') . lit 'd') `matches` "abbcd"
 -- Thanks to Mike Vanier for some Haskelly tips.
 
 import Data.List (nub)
 
-matches re = any accepts . foldl step [re Accept]
+data State = Accept | Expect Char [State]
+  deriving Eq
 
-data State = Accept | Expect Char State | Fork State State
-  deriving (Eq, Ord)
-
-accepts Accept       = True
-accepts (Expect _ _) = False
-accepts (Fork s1 s2) = accepts s1 || accepts s2
-
+matches re = any (Accept ==) . foldl step (re [Accept])
 step states c = nub $ concat $ map (after c) states
 
-after _ Accept            = []
-after c (Expect c' state) = if c' == c then [state] else []
-after c (Fork s1 s2)      = after c s1 ++ after c s2
+after _ Accept             = []
+after c (Expect c' states) = if c' == c then states else []
 
-lit = Expect
-alt re1 re2 state = re1 state `Fork` re2 state
-many re state = loop where loop = state `Fork` re loop
+lit c states = [Expect c states]
+alt re1 re2 states = re1 states ++ re2 states
+many re states = loop where loop = states ++ re loop
