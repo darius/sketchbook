9ba53b4a2ad2d5080d9ae935d0fe23c88e3cf605
Darius Bacon <darius@wry.me>
2016-05-18 12:42:36 -0400

    take out 'nub' to make it more like Thompson's code

--- regex/nfa.hs
+++ regex/nfa.hs
@@ -1,18 +1,18 @@
--- Like nfa.py
 -- E.g.: (lit 'a' . many (lit 'b' `alt` lit 'c') . lit 'd') `matches` "abbcd"
--- Thanks to Mike Vanier for some Haskelly tips.
-
-import Data.List (nub)
+-- Like Thompson: doesn't dedup the states, and loops forever on (a*)*.
+-- Unlike Thompson there's no 'fork' node in the compiled graph, which
+-- in consequence may be asymptotically bigger.
 
 data State = Accept | Expect Char [State]
   deriving Eq
 
-matches re = any (Accept ==) . foldl step (re [Accept])
-step states c = nub $ concat $ map (after c) states
-
 after _ Accept             = []
 after c (Expect c' states) = if c' == c then states else []
 
-lit c states = [Expect c states]
+step states c = concat $ map (after c) states
+
+re `matches` cs = Accept `elem` foldl step (re [Accept]) cs
+
+lit c       states = [Expect c states]
 alt re1 re2 states = re1 states ++ re2 states
-many re states = loop where loop = states ++ re loop
+many re     states = loop where loop = states ++ re loop
