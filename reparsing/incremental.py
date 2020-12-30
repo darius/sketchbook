7d1304ab82c40e443d08b1e74bfce0b033b57743
darius <withal@gmail.com>
2020-12-30 14:05:14 -0600

    extract common semantics

--- reparsing/incremental.py
+++ reparsing/incremental.py
@@ -3,6 +3,7 @@ Tweak nonincremental.py to incremental reparsing.
 """
 
 from metagrammar import parser_peg
+from semantics import *
 
 # Top level
 
@@ -84,43 +85,6 @@ class Parsing(object):
         return memo
 
 cyclic = object()
-
-
-# Varieties of semantics
-
-def DictSemantics(*dicts):
-    result = {}
-    for d in dicts: result.update(d)
-    return result
-
-base_semantics = DictSemantics(__builtins__)  # TODO Parson conveniences like `hug`
-
-def ModuleSemantics(module):
-    # TODO keep up with any updates to the module instead of just copying the dict
-    return DictSemantics(module.__dict__)
-
-class ASTSemantics(object):
-    def __getitem__(self, name):
-        return lambda *args: (name,) + args
-    def get(self, name, default=None):
-        return self[name]
-
-ast_semantics = ASTSemantics()
-
-class ComboSemantics(object): # TODO is there a dictlike class that already does this?
-    def __init__(self, *semanticses):
-        self.sems = list(semanticses)
-        self.sems.reverse()
-    def __getitem__(self, name):
-        value = self.get(name)
-        if value is None: raise KeyError(name)
-        return value
-    def get(self, name, default=None):
-        for sem in self.sems:
-            value = sem.get(name)
-            if value is not None:
-                return value
-        return default
 
 
 # Example
